ce4719b17b38d663249a6c56930e718d01e17fd9
trungtan <trungtan14@gmail.com>
2024-01-16 12:48:10 +0700

    add async/await when create server

--- server.ts
+++ server.ts
@@ -7,6 +7,12 @@ import http from "http"
 
 const server = http.createServer(app)
 
+// create server
+server.listen(process.env.PORT, async () => {
+  console.log(`Server is connected with port ${process.env.PORT}`)
+  await connectDB();
+})
+
 // cloudinary config
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -16,8 +22,3 @@ cloudinary.config({
 
 initSocketServer(server)
 
-// create server
-server.listen(process.env.PORT, () => {
-  console.log(`Server is connected with port ${process.env.PORT}`)
-  connectDB();
-})
